6bc28ece9411e542e337d242837900f042686367
dzenang <dzenan.granulo@gmail.com>
2024-08-09 16:36:18 +0200

    Add basic implementation

---
+++ src/test/java/granulo/dzenan/trading_analyzer/TradingAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package granulo.dzenan.trading_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
